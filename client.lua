6a8a57989bbf8f9dd6af8c240bd34a0c97c422cd
Bandit-code <75996046+Bandit-code@users.noreply.github.com>
2021-02-18 18:19:55 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,13 +1,11 @@
 Citizen.CreateThread(function()
 	while true do
-        --This is the client ID (Replace this with you own)
+
 		SetDiscordAppId(782652455552024577)
 
-        --This is the large icon put the name of it below
 		SetDiscordRichPresenceAsset('astrorp')
         
         
-        --This is the small icon put the name of it below
         SetDiscordRichPresenceAssetSmall('astrorp-2')
 
 
